08a9424a15334b6e2428aed2743dcd109fd044e2
Manoj Fernando <manojf@99x.lk>
2020-10-10 09:03:21 +0530

    Added lambda code

--- handler.js
+++ handler.js
@@ -1,15 +1,30 @@
 'use strict';
+const mongo = require('mongodb');
+const { MongoClient } = mongo;
+let db = null;
+
+let connectToDatabase = (uri, dbName) => {
+  if (db && db.serverConfig.isConnected()) {
+    return Promise.resolve(db);
+  }
+  return MongoClient.connect(uri, { poolSize: 10, useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+    db = client.db(dbName);
+    return db;
+  });
+};
+
+let getTodoById = (db, table, query) => {
+  return db
+    .collection(table)
+    .findOne(query);
+}
 
 module.exports.getData = async event => {
+  const dbConnection = await connectToDatabase("MONGODB_URI", "DB_NAME");
+  const todo = await getTodoById(dbConnection, "todos", { id: 100 });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(todo),
   };
 };
